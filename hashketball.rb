5126d12d3224f771ad10e7435855e0ddac7b85ab
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-06 17:47:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ end
 def num_points_scored(player_name)
    game_hash.each do |key, value|
      value.each do |inner_key, inner_value|
-       value[:players].each do 
+       value[:players].each do |players|
 end
 end
   
